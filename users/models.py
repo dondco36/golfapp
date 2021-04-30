812585f499f1d9947e6e9a8bea4f0a1793e7b59d
dondco36 <dondco36@gmail.com>
2021-04-29 18:46:43 -0700

    Styling almost complete

--- users/models.py
+++ users/models.py
@@ -7,12 +7,3 @@ from django.contrib.auth.models import AbstractUser, models
 
 class User(AbstractUser):
     id = models.AutoField(primary_key=True),
-
-    # def scoreAverager():
-    #     average_score = 0
-    #     for round in round_set:
-    #         average_score += round.score
-    #     average_score = average_score/count(round_set)
-    #     return average_score
-
-    # average_score = models.IntegerField(default=scoreAverager())
